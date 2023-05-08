558fc62dd88a730db72ab83f4f779627deb82aec
PengYu1990 <yp1990@gmail.com>
2023-05-08 21:36:46 +0100

    Refactor Code

--- src/PostDetail.tsx
+++ src/PostDetail.tsx
@@ -69,10 +69,6 @@ const PostDetail = () => {
     if (!data || !data.content) return "";
 
     return generateHTML(JSON.parse(data.content), [
-      // Document,
-      // Paragraph,
-      // Text,
-      // Bold,
       StarterKit,
       Underline,
       TiptapLink,
@@ -80,28 +76,8 @@ const PostDetail = () => {
       SubScript,
       Highlight.configure(),
       TextAlign,
-      // CodeBlockLowlight,
-      // other extensions …
     ]);
   }, [data?.content]);
-
-  if (!data) {
-    return (
-      <>
-        <Grid grow>
-          <Grid.Col md={9} sm={12}>
-            {isLoading && <PostDetailSkeleton />}
-          </Grid.Col>
-
-          <Grid.Col md={3} sm={12}>
-            <PublishBox />
-            {/* <HotPost /> */}
-          </Grid.Col>
-        </Grid>
-        <ToTop />
-      </>
-    );
-  }
 
   return (
     <>
@@ -109,8 +85,8 @@ const PostDetail = () => {
         <Grid.Col md={9} sm={12}>
           {isLoading && <PostDetailSkeleton />}
           <Box className={classes.detail}>
-            <h1 className={classes.heading}>{data.title}</h1>
-            <PostMeta post={data} />
+            <h1 className={classes.heading}>{data?.title}</h1>
+            {data && <PostMeta post={data} />}
             <div
               className={classes.content}
               dangerouslySetInnerHTML={{ __html: output }}
@@ -118,7 +94,7 @@ const PostDetail = () => {
           </Box>
           <Box className={classes.detail}>
             <CommentForm
-              postId={data.id}
+              postId={data?.id}
               addComment={(comment) =>
                 setCommentList([comment, ...commentList])
               }
