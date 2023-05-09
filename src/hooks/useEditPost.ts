f5ebb505c52bdadc8db54499a62521a5055a4882
PengYu1990 <yp1990@gmail.com>
2023-05-09 09:37:25 +0100

    Fix edit post page empty request

--- src/hooks/useEditPost.ts
+++ src/hooks/useEditPost.ts
@@ -1,6 +1,12 @@
 import { Post } from "./usePosts";
 import useSingleData from "./useSingleData";
 
-const useEditPost = (id:string | undefined) => useSingleData<Post>(`/api/posts/${id}`,[id])
+const useEditPost = (id:string | undefined) => {
+    if(!id){
+        const data = null;
+        return{data};
+    } 
+    return useSingleData<Post>(`/api/posts/${id}`,[id])
+}
 
 export default useEditPost
