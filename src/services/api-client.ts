da447d46daf3c8e4b2d42ff31099c1f32f296c78
PengYu1990 <yp1990@gmail.com>
2023-06-17 23:04:35 +0100

    update Bearer

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -5,17 +5,19 @@ import { getSessionUser } from "./session-service";
 const createClient = () =>{
     const user = getSessionUser();
     let token = null;
+    let authHeader = null;
     if(user != null){
         token = user.token;
+        authHeader = "Bearer " + token;
     }
     
     return axios.create({
-        baseURL:"https://itireland.herokuapp.com",
+        // baseURL:"https://itireland.herokuapp.com",
         // baseURL:"https://itireland.onrender.com",
-        // baseURL:"http://localhost:8080",
+        baseURL:"http://localhost:8080",
 
         headers:{
-            Authorization:token
+            Authorization:authHeader,
         }
     })
 }
