d7e01f304e0c64312cc91fad73e7d6ab2a92fa9f
PengYu1990 <yp1990@gmail.com>
2023-06-30 15:23:31 +0100

    test mobile responsiveness

--- src/components/shared/PostItem.tsx
+++ src/components/shared/PostItem.tsx
@@ -134,27 +134,21 @@ const PostItem = ({ post }: Props) => {
           </Link>
         </Flex>
         <Text className={classes.summary}>
-          <Spoiler maxHeight={120} showLabel="Show more" hideLabel="Hide">
-            <div
-              dangerouslySetInnerHTML={{
-                __html: generateHTML(JSON.parse(post.content), [
-                  // Document,
-                  // Paragraph,
-                  // TipTapText,
-                  // Bold,
-                  StarterKit,
-                  Underline,
-                  TiptapLink,
-                  Superscript,
-                  SubScript,
-                  Highlight.configure(),
-                  TextAlign,
-                  // CodeBlockLowlight,
-                  // other extensions …
-                ]),
-              }}
-            ></div>
-          </Spoiler>
+          {/* <Spoiler maxHeight={120} showLabel="Show more" hideLabel="Hide"> */}
+          <div
+            dangerouslySetInnerHTML={{
+              __html: generateHTML(JSON.parse(post.content), [
+                StarterKit,
+                Underline,
+                TiptapLink,
+                Superscript,
+                SubScript,
+                Highlight.configure(),
+                TextAlign,
+              ]),
+            }}
+          ></div>
+          {/* </Spoiler> */}
         </Text>
 
         <PostMeta post={post} />
